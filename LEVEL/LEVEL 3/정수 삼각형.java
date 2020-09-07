d577160b4b43126fe4c804ca82d84ce4e28e5576
eunbi14 <53552966+eunbi14@users.noreply.github.com>
2020-09-07 16:56:07 +0900

    Create 정수 삼각형.java

---
+++ LEVEL/LEVEL 3/정수 삼각형.java
@@ -0,0 +1,21 @@
+// 1 point
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int N = triangle.length;
+        int[][] answer = new int[N][N];
+        answer[0][0] = triangle[0][0];
+        for(int i=1;i<N;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0) answer[i][j] = answer[i-1][0] + triangle[i][j];
+                else answer[i][j] = Math.max(answer[i-1][j-1], answer[i-1][j]) + triangle[i][j];
+            }
+        }
+        
+        int out = 0;
+        for(int i=0;i<N;i++){
+            out = out>answer[N-1][i]?out:answer[N-1][i];
+        }
+        return out;
+    }
+}
